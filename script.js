50a77d3d0f63afe7839ca3cfb84ae5c1a3629338
Filip Timofiejew <ftimofiejew@gmail.com>
2020-08-12 19:05:23 +0200

    script.js modified

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://serene-falls-01416.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://serene-falls-01416.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://serene-falls-01416.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
